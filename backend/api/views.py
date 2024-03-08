0cd458b580f7637f315cf43ce34938f7f324ce59
itsobby <pascalobala@gmail.com>
2024-03-08 18:14:05 +0300

    Modified views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,19 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    print(request.GET) # url query params
+    print(request.POST) # form data
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dict 
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.headers # request.Meta ->
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
