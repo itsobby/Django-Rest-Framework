439adc3363b9021753a8723f497451a89e33ffec
itsobby <pascalobala@gmail.com>
2024-03-09 15:12:50 +0300

    Minor changes

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,8 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"product_id": 123 }) # HTTP Request
+get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
+# print(get_response.headers) # print raw text response
 # print(get_response.text) # print raw text response
 # print(get_response.status_code)
 
