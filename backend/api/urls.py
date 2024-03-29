8e6d9934fb1826cd48132197afcb908901d9aced
itsobby <pascalobala@gmail.com>
2024-03-29 23:03:13 +0300

    Worked on authentication with a token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 # from .views import api_home
 
 urlpatterns = [
+    path('auth/', obtain_auth_token),
     path('', views.api_home),
 ]
